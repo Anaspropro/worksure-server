70b03606bbce7753cdc666834f3dc1b375b63f86
Webwiz <111020302+Anaspropro@users.noreply.github.com>
2026-03-28 16:19:06 +0100

    Delete dist directory

--- dist/src/config/app-config.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { StringValue } from 'ms';
-export declare class AppConfigService {
-    get port(): number;
-    get databaseUrl(): string | undefined;
-    get isDatabaseConfigured(): boolean;
-    get jwtSecret(): string;
-    get jwtExpiresIn(): StringValue;
-}
--- dist/src/modules/wallet/wallet.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class WalletService {
-    getDefinition(): import("../../common/interfaces/domain-definition.interface").DomainDefinition | undefined;
-}
